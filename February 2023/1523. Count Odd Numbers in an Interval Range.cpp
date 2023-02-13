110a83cee9d39d8b232f9767ae9e499fc5d4d4b8
arundhatiimishra <74201615+arundhatiimishra@users.noreply.github.com>
2023-02-13 10:11:47 +0530

    13-02-2023

---
+++ February 2023/1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,12 @@
+// 1523. Count Odd Numbers in an Interval Range
+// Problem Link - https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+class Solution {
+public:
+    int countOdds(int low, int high){
+        if(low%2==0 && high%2==0){
+            return (high-low)/2;
+        }
+        return ((high-low)/2)+1; 
+    }
+};
